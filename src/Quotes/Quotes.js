ef6fca1559184a8baad526f7f5f62f050dabdd04
JordanLarson <jdlarson1234@gmail.com>
2020-06-28 20:19:03 -0700

    button functioning

--- src/Quotes/Quotes.js
+++ src/Quotes/Quotes.js
@@ -1,27 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
+let i = 1;
+
 const Quotes = (props) => {
-  const [quoteInfo, setQuoteInfo] = useState("");
-  const [myQuotes, setMyQuotes] = useState("");
+  const [quoteInfo, setQuoteInfo] = useState("data.value");
+  const [whoIsQuoted, setWhoIsQuoted] = useState("");
   const [urlData, setUrlData] = useState([]);
+
   useEffect(() => {
     const makeApiCall = async () => {
       const urlData = "https://api.tronalddump.io/random/quote";
       const res = await fetch(urlData);
       const data = await res.json();
       const quoteInfo = data.value;
+      const whoIsQuoted = data.tags[0];
       console.log(data);
       console.log(data.value);
       console.log(data.appeared_at);
+      console.log(data.tags[0]);
       console.log(quoteInfo);
+      setQuoteInfo(data.value);
     };
     makeApiCall();
-  }, [quoteInfo]);
+  }, [i]);
   return (
     <div>
-      <h5>yo: {quoteInfo} </h5>
       <h3> Donald's Funny Quotes </h3>
+      <h5 key={quoteInfo}> quote: {quoteInfo} </h5>
+      <h5 key={urlData}> referencing: {whoIsQuoted}</h5>
     </div>
   );
 };
