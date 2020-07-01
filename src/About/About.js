cbea008bcdecf4e9b7c50716618ad08b74942c97
JordanLarson <jdlarson1234@gmail.com>
2020-06-30 21:06:08 -0700

    fifth component

---
+++ src/About/About.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h3>about</h3>
+      <p>friendship and teamwork</p>
+    </div>
+  );
+};
+export default About;
